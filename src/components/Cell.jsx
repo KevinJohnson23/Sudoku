abce32ab483b46baa202211862cf0b90a4c7994d
Kevin Johnson <kevinJohnson190306@gmail.com>
2025-10-09 21:14:47 +0100

    Add keyboard input

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,3 +1,37 @@
+import { useState } from "react"
+
 export default function Cell() {
-  return <button className="cell">0</button>
+  const [value, setValue] = useState(0);
+
+  function isInputValid(newValue) {
+    if (!Number.isInteger(newValue)) {
+      return false
+    }
+    return newValue >= 0 && newValue <= 9
+  }
+
+  function handleInput(inputBox) {
+    let newValue = inputBox.value
+    newValue = newValue.slice(-1)
+    if (newValue === "") {
+      setValue(null)
+      return
+    }
+    newValue = parseInt(newValue)
+    if (isInputValid(newValue)) {
+      setValue(newValue)
+      inputBox.value = newValue
+    } else {
+      inputBox.value = value
+    }
+  }
+
+  return (
+    <input 
+      type="text" 
+      className="cell"
+      value={value}
+      onInput={e => handleInput(e.target)}
+    />
+  )
 }
