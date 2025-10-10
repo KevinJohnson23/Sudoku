3b177f57599e3d0bcd2270385a49f8f24faf07d3
Kevin Johnson <kevinJohnson190306@gmail.com>
2025-10-10 23:33:01 +0100

    .css to .scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import "./App.css"
+import "./App.scss"
 import Grid from "./components/Grid"
 import CompletionScreen from "./components/CompletionScreen"
 import generate from "./sudoku/generator"
