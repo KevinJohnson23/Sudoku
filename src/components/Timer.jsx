c3eede1812683f77ee5ecf968e3435cdcdfe4545
Kevin Johnson <kevinJohnson190306@gmail.com>
2025-10-11 15:24:03 +0100

    Format time to hh:mm:ss

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,30 @@
+function fixSingleDigits(number) {
+  let numberString = number.toString()
+  if (numberString.length == 1) {
+    return "0" + numberString
+  }
+  return numberString
+}
+
+function formatTime(seconds) {
+  const hours = Math.floor(seconds / 3600)
+  seconds %= 3600
+  const minutes = Math.floor(seconds / 60)
+  seconds %= 60
+
+  if (hours > 0) {
+    return `${hours}:${fixSingleDigits(minutes)}:${fixSingleDigits(seconds)}`
+  } else if (minutes > 0) {
+    return `${minutes}:${fixSingleDigits(seconds)}`
+  } else {
+    return `0:${fixSingleDigits(seconds)}`
+  }
+}
+
 export default function Timer({ timeSeconds }) {
   return (
     <div>
-      Time: {timeSeconds}
+      Time: {formatTime(timeSeconds)}
     </div>
   )
 }
