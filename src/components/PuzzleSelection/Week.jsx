e65628eab1cd99d7fcfec90a40ed9a879c789179
Kevin Johnson <kevinJohnson190306@gmail.com>
2025-10-11 19:00:07 +0100

    Cleanup old code

--- src/components/PuzzleSelection/Week.jsx
+++ src/components/PuzzleSelection/Week.jsx
@@ -21,28 +21,4 @@ export default function Week({ year, month, row, setDate }) {
       ))
     }
   </>
-
-  // if (row < 4) {
-  //   return (
-  //     <>
-  //       <td><button>{dayNum + 1}</button></td>
-  //       <td><button>{dayNum + 2}</button></td>
-  //       <td><button>{dayNum + 3}</button></td>
-  //       <td><button>{dayNum + 4}</button></td>
-  //       <td><button>{dayNum + 5}</button></td>
-  //       <td><button>{dayNum + 6}</button></td>
-  //       <td><button>{dayNum + 7}</button></td>
-  //     </>
-  //   )
-  // } else {
-  //   const lastDay = new Date(year, month + 1, 0).getDate()
-  //   const numDays = lastDay - dayNum
-  //   return (
-  //     <>
-  //       {Array(7).fill(0).map((_, i) => (
-  //         <td key={i}><button>{i < numDays ? dayNum + i + 1 : i - numDays + 1}</button></td>
-  //       ))}
-  //     </>
-  //   )
-  // }
 }
