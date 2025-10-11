cba220f12b9c492d92cfe90642b68adef88e6240
Kevin Johnson <kevinJohnson190306@gmail.com>
2025-10-11 14:39:03 +0100

    Add text to completion screen

--- src/components/CompletionScreen.jsx
+++ src/components/CompletionScreen.jsx
@@ -4,6 +4,12 @@ export default function CompletionScreen({ isComplete, newPuzzle }) {
   return isComplete ? (
     <div className="overlay">
       <div className="center-overlay">
+        <h1>
+          Puzzle Complete!
+        </h1>
+        <p className="completion-text">
+          You took hh:mm:ss to finish
+        </p>
         <button
           className="play-again-button"
           onClick={newPuzzle}
